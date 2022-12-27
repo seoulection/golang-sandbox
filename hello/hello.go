2b52bb84251df22924c44b37e61e194f1ef2ff19
seoulection <6445410+seoulection@users.noreply.github.com>
2022-12-27 08:21:29 -0800

    create a go module

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
 }
